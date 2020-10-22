8181cf57387e8756a59c74101c8faa292b34ed1a
Sergey Malyshev <malyshevs_v@mail.ru>
2020-10-22 23:57:26 +0300

    Class Insert

--- src/ru/smal/Insert.java
+++ src/ru/smal/Insert.java
@@ -4,22 +4,24 @@ import java.sql.*;
 
 public class Insert {
     private static final String DB_DRIVER = "com.mysql.cj.jdbc.Driver";
-    private static final String DB_CONNECTION ="jdbc:mysql://localhost:3306/users?serverTimezone=UTC";
+    private static final String DB_CONNECTION = "jdbc:mysql://localhost:3306/users?serverTimezone=UTC";
     private static final String DB_USER = "user1";
     private static final String DB_PASSWORD = "12345";
 
     public static void main(String[] args) throws ClassNotFoundException {
+
         Class.forName(DB_DRIVER);
 
         try(Connection connection = DriverManager.getConnection(DB_CONNECTION, DB_USER, DB_PASSWORD)){
-            System.out.println("Connection ON");
 
             connection.setAutoCommit(false);
+            connection.setTransactionIsolation(Connection.TRANSACTION_SERIALIZABLE);
+
             String sqlInsert = "INSERT INTO user (firstName, lastName, age) VALUES (?, ?, ?)";
 
             PreparedStatement statement = connection.prepareStatement(sqlInsert, Statement.RETURN_GENERATED_KEYS);
-            statement.setString(1, "Idey");
-            statement.setString(2, "Ideev");
+            statement.setString(1, "Client");
+            statement.setString(2, "Clientov");
             statement.setInt(3, 10);
             int res = statement.executeUpdate();
 
@@ -28,7 +30,7 @@ public class Insert {
                 ResultSet result = statement.getGeneratedKeys();
                 if(result.next()){
                     int id = result.getInt(1);
-                    System.out.printf("Пользователь добавлен Id = %d", id);
+                    System.out.printf("Пользователь добавлен под номером: %d", id);
                 }
             }
 
@@ -36,7 +38,6 @@ public class Insert {
 
         } catch (SQLException throwables) {
             throwables.printStackTrace();
-            System.out.println("Connection failed");
         }
 
 
